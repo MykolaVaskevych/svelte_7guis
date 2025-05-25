78c5c0349a6fee11dfa35133e4acf1adf1536b41
Mykola Vaskevych <22372199@studentmail.ul.ie>
2025-05-25 10:47:13 +0100

    added layout to use in chalanges

---
+++ src/lib/components/challengeLayout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+    let {children, title, challenge, desription, image} = $props();
+
+</script>
+
+<div class="flex h-screen w-screen flex-row">
+    <div class="bg-base-100 flex flex-1 flex-col items-center justify-center gap-4">
+        {@render children()}
+    </div>
+
+
+    <div class="bg-base-200 flex flex-1 flex-col items-center justify-center p-8">
+        <div class="max-w-2xl space-y-4">
+            <p class="text-2xl font-bold">{title}</p>
+            <p class="text-secondary text-lg font-semibold">{challenge}</p>
+            {#if image}
+                <img src={image} alt="example" class="mx-auto">            
+            {/if}
+            <p class="text-base leading-relaxed">{desription}</p>
+        </div>
+    </div>
+</div>
